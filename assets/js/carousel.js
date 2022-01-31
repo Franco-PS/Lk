57e5606bb9e51a75c0efa6ae99351c6079213f9a
FranckL <perez_salazarf@hotmail.com>
2022-01-31 04:40:51 -0600

    first commit

---
+++ assets/js/carousel.js
@@ -0,0 +1,13 @@
+const boxCarousel= document.getElementById('container__carousel');
+
+setInterval(function(){
+    let carousel= document.querySelectorAll('.carousel-card')[0];
+    console.log(carousel)
+    boxCarousel.style.left="calc(var(--width-carousel) *-1)";
+    boxCarousel.style.transition="all 2s";
+    setTimeout(function(){
+        boxCarousel.insertAdjacentElement('beforeend', carousel);
+        boxCarousel.style.transition="none";
+        boxCarousel.style.left="0";
+    }, 2000)
+}, 3900)
